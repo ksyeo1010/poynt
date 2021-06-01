4d00a47a86afada5ec113d14dcea4b52aa628d16
Kwangsoo <ksyeo1010@gmail.com>
2021-05-31 22:32:04 -0700

    initial discord bot setup

---
+++ bot.py
@@ -0,0 +1,40 @@
+import os
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+
+def main():
+    @bot.event
+    async def on_ready():
+        print('Logged in as')
+        print(bot.user.name)
+        print(bot.user.id)
+        print('------')
+
+    @bot.command()
+    async def add(ctx, left: int, right: int):
+        """Adds two numbers together."""
+        await ctx.send(left + right)
+
+    bot.run(TOKEN)
+
+
+if __name__ == '__main__':
+    main()
