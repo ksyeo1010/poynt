a69cc01f3874112301616d9b4558a2bd61d2b46a
Paul Seong Uk Yeon <syeon@my.bcit.ca>
2021-06-01 20:08:04 -0700

    implement some functionality for db testing

--- bot.py
+++ bot.py
@@ -46,9 +46,9 @@ def main():
         user_points = UserController.get_user_points(ctx)
         await ctx.send("You currently have " + str(user_points))
 
-    @bot.command()
+    @bot.command(name="predict")
     @commands.has_permissions(administrator=True)
-    async def predict(ctx, round_title: str, *args):
+    async def _predict(ctx, round_title: str, *args):
         """$predict <round_title> <options> ... <options>"""
         UserController.add_round(ctx, round_title)
         for choice in args:
@@ -62,6 +62,12 @@ def main():
             message = UserController.print_predictions(choice, counter)
             await ctx.send(message)
 
+    @_predict.error
+    async def predict_error(ctx, error):
+        # move this to a UserController.no_permission_error
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"Sorry {ctx.message.author}, you are not an admin!")
+
     @bot.command()
     async def bet(ctx, round_title: str, bet_choice: str, bet_amount: int):
         """$bet <round_title> <option_name> <bet_amount>"""
@@ -72,38 +78,119 @@ def main():
             UserController.bet_points(ctx, round_title, bet_choice, str(ctx.message.author), bet_amount)
 
     @bot.command()
+    async def activerounds(ctx):
+        # which rounds are active right now
+        pass
+
+    @bot.command()
     async def multiplier(ctx, round_title: str, amount: int):
         """Return the multiplier for each option for the round"""
         # await ctx.send("This is the current multiplier for each choice!")
-        # for each in range(1, RoundController.get_total_choices() + 1):
-        #   for round in RoundController.get_round(ctx, round_title, each)
-        #       multiplier = RoundController.get_multiplier(ctx, round_title, winning_choice)
-        #       await ctx.send(f"The multiplier for option {each} is {multiplier}. "
-        #                      f"This means if you bet {amount} points, you'll get back {amount * multiplier} plus the "
-        #                      f"original bet amount of {amount}, with the total of {(amount * multiplier) + amount}")
+        # multiplier_message = UserController.multipliers_for_round(ctx, round_title, amount)
+        # await ctx.send(multiplier_message)
         pass
 
-    @bot.command()
+    @bot.command(name="payout")
     @commands.has_permissions(administrator=True)
-    async def payout(ctx, round_title: str, winning_choice: str):
+    async def _payout(ctx, round_title: str, winning_choice: str):
         """$payout <round_title> <winning_choice>"""
-        # winner_list = RoundController.get_round(ctx, round_title, winning_choice)
-        # multiplier = RoundController.get_multiplier(ctx, round_title, winning_choice)
-        # for player in winner_list:
-        #   winnings = (player.bet_amount * multiplier) + player.bet_amount
-        #    UserController.update_points(ctx, winnings)
-        # await ctx.send("All the points have been distributed!")
-        # RoundController.delete_round(ctx, round_id)
+        # multiplier = UserController.multiplier(ctx, round_title)
+        # UserController.payout_round(ctx, round_title, winning_choice)
+        # winner_list = RoundController.get_winners(ctx, round_title, winning_choice)
+        # await ctx.send(winner_list)
+        # UserController.delete_round(ctx, round_title)
+        pass
+
+    @_payout.error
+    async def payout_error(ctx, error):
+        # move this to a UserController.no_permission_error
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"Sorry {ctx.message.author}, you are not an admin!")
+
+    @bot.command(name="goodbyepoynt")
+    @commands.has_permissions(administrator=True)
+    async def _goodbyepoynt(ctx):
+        # move code below to a UserController and call controller instead
+        # await ctx.send(f"Are you sure you would kick poynt?\n"
+        #                f"This will remove all data stored up to this point.\n"
+        #                f"Type yes or no to confirm.")
+        #
+        # def check(message):
+        #     return message.author == ctx.author and message.channel == ctx.channel \
+        #            and message.content.lower() in ["yes", "no"]
+        #
+        # response = await bot.wait_for("message", check=check)
+        # if response.content.lower() == "yes":
+        #     This controller will kick poynt and delete the database and also send message for completion
+        #     UserController.kick_poynt(guild_id)
+        # elif response.content.lower() == "no":
+        #     await ctx.send(f"Thanks for not kicking me! :)")
+        # else:
+        #     await ctx.send(f"That's not a valid input, run the command again!")
+        pass
+
+    @_goodbyepoynt.error
+    async def goodbyepoynt_error(ctx, error):
+        # move this to a UserController.no_permission_error
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"Sorry {ctx.message.author}, you are not an admin!")
+
+
+    @bot.command()
+    async def ranking(ctx, category: str):
+        # category will be wins, losses, or points
+        # controller will return the list of ranked players for a category
+        # result = UserController.get_ranking(ctx, category)
+        # await ctx.send(result)
         pass
 
     @bot.command()
-    async def ranking(ctx):
-        # users = vars(UserController.get_all_users(ctx))
-        #
-        #
-        # for user in range(len(users)):
-        #
+    async def shop(ctx):
+        # display shop for users
+        # shop_list = UserController.display_shop(ctx)
+        # await ctx.send(shop_list)
         pass
+
+    @bot.command()
+    async def use(ctx, item: str):
+        # uses item from the user's list of items
+        # if item count is less than 1, send error message
+        # controller subtracts count from the user's items and announces to the text channel about the usage
+        # message = UserController.use_item(ctx, item)
+        # await ctx.send(message)
+        pass
+
+    @bot.command()
+    async def cleaninventory(ctx):
+        # cleans item list by deleting items that have 0 item count
+        # UserController.clean_inventory(ctx)
+        pass
+
+    @bot.command()
+    async def buy(ctx, item: str):
+        # buys an item from the shop
+        # removes inventory count from shop and places it into user items list with item and count
+        # UserController.buy_item(ctx, item)
+        # announce the inventory left for the item
+        # message = UserController.announce_item_inventory(ctx, item)
+        # await ctx.send(message)
+        pass
+
+    @bot.command(name="shopadd")
+    @commands.has_permissions(administrator=True)
+    async def _shopadd(ctx, item: str, inventory: int,  description: str, price: int):
+        # able to add items to the shop
+        # inventory has to be greater than 0
+        # announce the item addition to server
+        # message = UserController.add_shop_items(ctx, inventory, stock description, price)
+        # await ctx.send(message)
+        pass
+
+    @_shopadd.error
+    async def shopadd_error(ctx, error):
+        # move this to a UserController.no_permission_error
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"Sorry {ctx.message.author}, you are not an admin!")
 
     @bot.command()
     async def add(ctx, left: int, right: int):
--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,4 +1,4 @@
-from src.modules.service import Service
+from src.modules.services import CommonService, RoundService, UserService
 
 
 class UserController:
@@ -6,36 +6,44 @@ class UserController:
     @staticmethod
     def add_users(ctx):
         guild_id = ctx.owner.guild.id
-        Service.init_guild(guild_id)
+        CommonService.init_guild(guild_id)
         for member in ctx.members:
-            Service.add_user(guild_id, str(member))
+            UserService.add_user(guild_id, str(member))
 
     @staticmethod
     def add_new_user(ctx):
         guild_id = ctx.owner.guild.id
-        Service.add_user(guild_id, str(ctx))
+        UserService.add_user(guild_id, str(ctx))
 
     @staticmethod
     def get_user_points(ctx):
         guild_id = ctx.guild.id
-        user = Service.get_user(guild_id, str(ctx.author))
+        user = UserService.get_user(guild_id, str(ctx.author))
         return user.points
 
     @staticmethod
     def bet_points(ctx, title, bet_choice, username, amount):
         guild_id = ctx.guild.id
-        Service.add_bet(guild_id, title, bet_choice, username, amount)
+        UserService.decrement_bet(guild_id, username, amount)
+        RoundService.add_bet(guild_id, title, bet_choice, username, amount)
 
     @staticmethod
     def add_round(ctx, round_title):
         guild_id = ctx.guild.id
-        Service.add_round(guild_id, round_title)
+        RoundService.add_round(guild_id, round_title)
 
     @staticmethod
     def add_choice(ctx, round_title, choice):
         guild_id = ctx.guild.id
-        Service.add_choice(guild_id, round_title, choice)
+        RoundService.add_choice(guild_id, round_title, choice)
 
     @staticmethod
     def print_predictions(choice, counter):
         return f"{counter}: {choice}"
+
+    # @staticmethod
+    # def payout_round(ctx, round_title, winning_choice):
+
+    # @staticmethod
+    # def multiplier(ctx, round_title):
+    #
