94f708d471fb13efe4f9d1b9c1e8480fb9b109a6
Kwangsoo <ksyeo1010@gmail.com>
2021-06-03 21:34:06 -0700

    test env

--- scripts/set_environment.sh
+++ scripts/set_environment.sh
@@ -4,4 +4,5 @@ cd /home/ec2-user/poynt
 rm .env -y
 
 # add params
-echo DISCORD_GUILD=$(aws ssm get-parameters --output text --region us-east-1 --names DISCORD_GUILD --with-decryption --query Parameters[0].Value) >> .env
+echo DISCORD_GUILD=$(aws ssm get-parameters --output text --region us-east-1 --names DISCORD_GUILD --query Parameters[0].Value) >> .env
+echo DISCORD_GUILD=$(aws ssm get-parameters --names DISCORD_GUILD) >> .env
