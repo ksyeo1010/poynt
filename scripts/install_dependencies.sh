e119b300b9ea3e21fa4f503429b23b2f3f2ed2ae
Kwangsoo <ksyeo1010@gmail.com>
2021-06-03 23:33:40 -0700

    changed pip from sudo to non sudo

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,7 +8,7 @@ sudo cp ~/.env .
 # python
 virtualenv environment
 source environment/bin/activate
-sudo pip install -r requirements.txt
+pip install -r requirements.txt
 
 # mongodb
 docker-compose build
