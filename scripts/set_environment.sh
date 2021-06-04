ec1c98d874fd9b1bfcd3578b4650b7655a064f79
Kwangsoo <ksyeo1010@gmail.com>
2021-06-03 21:58:13 -0700

    set env variables

--- scripts/set_environment.sh
+++ scripts/set_environment.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/poynt
-
-rm .env -y
 
 # add params
-echo DISCORD_GUILD=$(aws ssm get-parameters --output text --region us-east-1 --names DISCORD_GUILD --query Parameters[0].Value) >> .env
-echo DISCORD_GUILD=$(aws ssm get-parameters --names DISCORD_GUILD) >> .env
+echo DISCORD_GUILD=${DISCORD_GUILD} >> .env
